2869b4cf7229ae6c1afcc9d87d27f096bd575edf
Javi Gomez <rtomyj@gmail.com>
2019-07-04 10:09:25 -0500

    Changed styling
    Card summary have a lighter color to distinguish the portion with the rest of the card details. Added padding/margin changes

--- src/card_detail.js
+++ src/card_detail.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 
+//import 'typeface-roboto'
+
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 
 import Typography from '@material-ui/core/Typography'
-import { blue } from '@material-ui/core/colors'
+import Divider from '@material-ui/core/Divider'
+import Grid from '@material-ui/core/Grid'
 import { withStyles } from '@material-ui/styles'
 
 import PropTypes from 'prop-types'
 
+import { blue } from '@material-ui/core/colors'
+import Box from '@material-ui/core/Box'
+
 const styles = {
 	linkCard: {
-		background: 'linear-gradient(45deg, #1e88e5 30%, #1565c0 90%)'
+		background: 'linear-gradient(45deg, #1e88e5 30%, #1565c0 90%)',
+		color: 'f5f5f5'
+	},
+	linkCardSummary: {
+		background: blue[400],
+		padding: '10'
+	},
+	cardTop: {
+		marginBottom: '15'
 	}
 }
 
@@ -20,14 +34,20 @@ class CardDetail extends Component
 	render()
 	{
 		const { classes } = this.props
-		return(
-			<Card>
-				<CardContent className={classes.linkCard}>
-					<Typography variant='subtitle1'>{this.props.cardName}</Typography>
-					<Typography variant='subtitle2'>{this.props.cardInfo}</Typography>
-					<Typography variant='body2'>{this.props.cardEffect}</Typography>
-				</CardContent>
-			</Card>
+		return (
+			<Grid item xs={4} >
+				<Card>
+					<CardContent className={classes.linkCard}>
+						<Box className={classes.cardTop}>
+							<Typography variant='subtitle1'>{this.props.cardName}</Typography>
+						</Box>
+						<Box className={classes.linkCardSummary} >
+							<Typography variant='subtitle2'>{this.props.cardInfo}</Typography>
+							<Typography variant='body2'>{this.props.cardEffect}</Typography>
+						</Box>
+					</CardContent>
+				</Card>
+			</Grid>
 		)
 	}
 }
