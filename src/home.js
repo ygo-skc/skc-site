b2ac85ae1358919233a862ad2baaac07588c6d25
Javi Gomez <rtomyj@gmail.com>
2019-08-18 15:15:56 -0500

    Started work on splash page

--- src/home.js
+++ src/home.js
@@ -1,8 +1,6 @@
 
 import React, { Component } from 'react'
 
-import BreadCrumb from './breadcrumb'
-
 import { Typography, Link, Paper } from '@material-ui/core'
 
 
@@ -18,13 +16,13 @@ class Home extends Component {
 
 	render() {
 		return (
-			<div style={{height: '100%'}} >
-				<Paper style={{verticalAlign: 'middle', padding: 20}} >
-					<Typography variant='h3'>
-						Website Name
+			<div style={{ height: '100%', 'WebkitAlignItems': 'center', 'WebkitBoxAlign': 'center', 'justifyContent': 'center', display: 'flex' }} >
+				<Paper style={{ padding: 40, marginLeft: 30, marginRight: 30, marginBottom: 450, width: '100%'}} >
+					<Typography variant='h4'>
+						Yugioh Website
 					</Typography>
 					<Typography variant='h6' style={{marginTop: 20}} >
-						Check out the <Link href={'/ban_list'} >Ban List</Link>
+						Check out the latest <Link href={'/ban_list'} >Ban List</Link> - last updated July.
 					</Typography>
 				</Paper>
 			</div>
