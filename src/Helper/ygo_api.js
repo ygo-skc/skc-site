7bb438bc05b36377de123883c0b7f217bf471604
Javi Gomez <rtomyj@gmail.com>
2019-10-06 19:50:39 -0500

    Easier host switching

--- src/Helper/ygo_api.js
+++ src/Helper/ygo_api.js
@@ -1,7 +1,9 @@
+const host = 'http://localhost:9999'
+
 const NAME_maps_ENDPOINT = {
-	'banListsUrl':					'http://localhost:9999/api/v1/ban_list_dates/',
-	'banListInstanceUrl':		'http://localhost:9999/api/v1/banned_cards/',
-	'cardInstanceUrl':			'http://localhost:9999/api/v1/card/'
+	'banListsUrl':					`${host}/api/v1/ban_list_dates/`,
+	'banListInstanceUrl':		`${host}/api/v1/banned_cards/`,
+	'cardInstanceUrl':			`${host}/api/v1/card/`,
 }
 
 export default NAME_maps_ENDPOINT
