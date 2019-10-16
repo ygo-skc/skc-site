b79256d2a6daa98fa48057aa7cd84d689ab0bcc0
Javi Gomez <rtomyj@gmail.com>
2019-10-15 21:29:23 -0500

    Updated 404 not found error page

--- src/Component/Exceptions/not_found.js
+++ src/Component/Exceptions/not_found.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function NotFound()
+import CardDetail from '../Card/card_detail'
+import BreadCrumb from '../breadcrumb.js'
+
+function NotFound(props)
 {
+	const CenteredContent = styled.div`
+		height: 100%;
+		-webkit-align-items: center;
+		-webkit-box-align: center;
+		justify-content: center
+		display: flex
+	`
 	return(
 		<div>
-			<p>hello world</p>
+			<BreadCrumb crumbs={['Home', '404 - Err']}/>
+			<CenteredContent>
+				<CardDetail style={{ 'max-width': '550px', 'min-width': '550px' }} cardColor='err' cardName='404 Not Found' monsterType='Client Error' cardEffect={`Requested webpage - ${window.location.href} - does not exist. \n\nUse the navigation to get back to an existing page.`} cardID='00000400' />
+			</CenteredContent>
 		</div>
 	)
 }
