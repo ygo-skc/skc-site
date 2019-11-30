1b31664a6ee01aec21bd05905c470ca5b79da1f7
Javi Gomez <rtomyj@gmail.com>
2019-11-30 11:33:15 -0600

    Updated padding and added icon to About route.

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -6,6 +6,7 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 import HomeIcon from '@material-ui/icons/Home'
 import Block from '@material-ui/icons/Block'
 import ErrorIcon from '@material-ui/icons/Error'
+import InfoIcon from '@material-ui/icons/Info';
 
 import { NAME_maps_ROUTE } from '../Routes'
 
@@ -15,10 +16,10 @@ const BreadCrumbsContent = styled(Paper)`
 	&&
 	{
 		display: flex;
-		padding: .9rem;
 		margin-top: .5rem;
 		margin-bottom: 1rem;
 		border-radius: .5rem;
+		padding: 1rem;
 	}
 
 `
@@ -37,6 +38,7 @@ const iconStyle = { width: 20, height: 20, marginRight: 5, color: 'rgba(68, 68, 
 BREADCRUMB_maps_ICON['Home'] = <HomeIcon style={iconStyle} />
 BREADCRUMB_maps_ICON['Ban List'] = <Block style={iconStyle} />
 BREADCRUMB_maps_ICON['404 - Err'] = <ErrorIcon style={iconStyle} />
+BREADCRUMB_maps_ICON['About'] = <InfoIcon style={iconStyle} />
 
 
 export default function BreadCrumb(props)
