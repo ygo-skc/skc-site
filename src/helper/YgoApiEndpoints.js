a731765e1a25296d2ad30ec8d377b2436212a051
Javi Gomez <rtomyj@gmail.com>
2019-12-17 01:10:26 -0600

    Updated endpoints

--- src/helper/YgoApiEndpoints.js
+++ src/helper/YgoApiEndpoints.js
@@ -1,9 +1,11 @@
-//const host = 'http://localhost:9999'
-const host = 'https://ygoapi.cfapps.io'
+const host = 'http://localhost:9999'
+//const host = 'https://ygoapi.cfapps.io'
+//const host = 'https://ygoapi-dev.cfapps.io'
 
 const NAME_maps_ENDPOINT = {
-	'banListsUrl':				`${host}/api/v1/ban_list_dates/`,
-	'banListInstanceUrl':		`${host}/api/v1/banned_cards/`,
+	'banListsUrl':				`${host}/api/v1/ban/dates/`,
+	'banListInstanceUrl':		`${host}/api/v1/ban/cards/`,
+	'newCardsInBanList':			`${host}/api/v1/ban/new/`,
 	'cardInstanceUrl':			`${host}/api/v1/card/`,
 }
 
