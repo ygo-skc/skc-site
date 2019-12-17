177a892c8fee512d6244eea141db07b492c83f52
Javi Gomez <rtomyj@gmail.com>
2019-12-17 13:13:05 -0600

    Using env vars for host name of API

--- src/helper/YgoApiEndpoints.js
+++ src/helper/YgoApiEndpoints.js
@@ -1,12 +1,10 @@
-const host = 'http://localhost:9999'
-//const host = 'https://ygoapi.cfapps.io'
-//const host = 'https://ygoapi-dev.cfapps.io'
+const API_HOST = process.env.REACT_APP_API_HOST
 
 const NAME_maps_ENDPOINT = {
-	'banListsUrl':				`${host}/api/v1/ban/dates/`,
-	'banListInstanceUrl':		`${host}/api/v1/ban/cards/`,
-	'newCardsInBanList':			`${host}/api/v1/ban/new/`,
-	'cardInstanceUrl':			`${host}/api/v1/card/`,
+	'banListsUrl':				`${API_HOST}/api/v1/ban/dates/`,
+	'banListInstanceUrl':		`${API_HOST}/api/v1/ban/cards/`,
+	'newCardsInBanList':			`${API_HOST}/api/v1/ban/new/`,
+	'cardInstanceUrl':			`${API_HOST}/api/v1/card/`,
 }
 
 export default NAME_maps_ENDPOINT
