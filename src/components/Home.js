0883108fc3b93b67b367933f2102588a988c1a5c
Javi Gomez <rtomyj@gmail.com>
2020-06-08 21:16:27 -0500

    Updated index page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Typography } from '@material-ui/core'
+import { Typography, Grid, Link } from '@material-ui/core'
 import { MainContentContainer, ChildPaper } from './MainContent'
 import Breadcrumb from './Breadcrumb'
 
@@ -26,42 +26,56 @@ export default function Home()
 		<MainContentContainer>
 			<Breadcrumb crumbs={['Home']} />
 
-			<ChildPaper>
-				<CenteredText variant='h4' >
-					Website Change Log
-				</CenteredText>
-				<br />
+			<Grid container spacing={2} >
+				<Grid item xs={12} sm={12} md={7} lg={8} xl={9} >
+					<ChildPaper>
+						<CenteredText variant='h4' >
+							Welcome Duelist
+						</CenteredText>
 
-				<Typography variant='subtitle2' >
-					&bull; <strong>March 21, 2020:</strong> Removed unneeded / simplified fonts to improve load times. Decoupled React.js resources to improve loading.
-				</Typography>
-				<Typography variant='subtitle2' >
-					&bull; <strong>January 20, 2020:</strong> The site has slight new look - updated fonts and colors. There are slight performance improvements to page loading.
-				</Typography>
-				<Typography variant='subtitle2'>
-					&bull; <strong>Dec 25, 2019:</strong> Site launch!
-				</Typography>
-			</ChildPaper>
+						<Typography variant='h6' >
+							This is the Supreme Kings Castle. The Supreme King is the most formidable duelist you will ever meet. His castle has many secrets waiting to be explored.
+						</Typography>
 
-			<ChildPaper>
-				<CenteredText variant='h4' >
-					API Change Log
-				</CenteredText>
-				<br />
+						<br /><br />
 
-				<Typography variant='subtitle2' >
-					&bull; <strong>March 26, 2020:</strong> Added data for next ban list (04/01/2020).
-				</Typography>
-				<Typography variant='subtitle2' >
-					&bull; <strong>March 21, 2020:</strong> Updated caching mechanism to reduce latency from DB to client. Added client level caching to minimize API to client requests. Updated implementations to increase code quality.
-				</Typography>
-				<Typography variant='subtitle2' >
-					&bull; <strong>January 20, 2020:</strong> Jan 2020 ban list has been added. Updates to include more info about new/removed cards.
-				</Typography>
-				<Typography variant='subtitle2'>
-					&bull; <strong>Dec 25, 2019:</strong> API launch!
-				</Typography>
-			</ChildPaper>
+						<CenteredText variant='h4' >
+							Content
+						</CenteredText>
+
+						<Typography variant='h6' >
+							There are <strong>986</strong> cards currently in the database. There are also <Link color='secondary' href='/ban_list'><strong>11</strong></Link> ban lists from the past 3 years to browse.
+						</Typography>
+						<Typography variant='h6' >
+							Yugioh is ever expanding and evolving. New products are continuously released and new ban lists established. As such this website will also  be continuously updated to accommodate.
+							Enjoy the website ad free with a fast and beautiful UI. There is no tracking and the only money we make is though donations. Want to become a patreon or learn more? Check out the <Link color='secondary' href='/ban_list'><strong>About</strong></Link> section
+						</Typography>
+					</ChildPaper>
+				</Grid>
+
+				<Grid item xs={12} sm={12} md={5} lg={4} xl={3} >
+					<ChildPaper>
+						<CenteredText variant='h4' >
+							Whats New?
+						</CenteredText>
+
+						<Typography variant='subtitle2' >
+							<strong>March 26, 2020:</strong> New ban list - content added for it.
+						</Typography>
+						<Typography variant='subtitle2' >
+							<strong>March 21, 2020:</strong> The Castle was cleaned up - navigating it is faster. The Database was updated to improve load times The Supreme King expects.
+						</Typography>
+						<Typography variant='subtitle2' >
+							<strong>January 20, 2020:</strong> New ban list - content added for it. The Castle got a slight change in looks.
+						</Typography>
+						<Typography variant='subtitle2'>
+							<strong>Dec 25, 2019:</strong> The Supreme King allows guests to The Castle.
+						</Typography>
+					</ChildPaper>
+					<CenteredText variant='subtitle2'>Copyright 2020</CenteredText>
+					<CenteredText variant='subtitle2'>Konami Digital Entertainment owns all rights to Yu-Gi-Oh!</CenteredText>
+				</Grid>
+			</Grid>
 		</MainContentContainer>
 	)
 }
