4398d4708371f0ebdf8e0c2d571077e85793ae74
Javi Gomez <rtomyj@gmail.com>
2020-08-02 11:56:18 -0500

    Component for rendering card level based on props

---
+++ src/components/card/CardLevel.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+
+const levelImage = <img src={`${process.env.PUBLIC_URL}/Img/card_level.svg`} style={{ width: '20px', marginLeft: '1px' }} />
+
+
+const CardLevel = ( { level } ) =>
+{
+	const [levelImages, setLevelImages] = useState(undefined)
+	useEffect( () => {
+		let starsToCreate = Number(level)
+		const stars = []
+
+		while (starsToCreate != 0)
+		{
+			stars.push(levelImage)
+			starsToCreate--
+		}
+		setLevelImages(stars)
+
+	}, level)
+
+
+	return(
+		<div style={{ width: '100%', marginBottom: '.25rem', textAlign: 'right'}} >
+			{ levelImages }
+		</div>
+	)
+}
+
+export { CardLevel }
