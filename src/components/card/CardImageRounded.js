090b2994eb9be6e9258528c469beaf768ad4a1d8
Javi Gomez <rtomyj@gmail.com>
2020-11-25 19:10:16 -0600

    Lazy loading of images

--- src/components/card/CardImageRounded.js
+++ src/components/card/CardImageRounded.js
@@ -1,29 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import VizSensor from 'react-visibility-sensor'
 
+async function getImg(imgURL)
+{
+	return <img
+		src={imgURL}
+		style={{  width: '100%', objectFit: 'cover' }}
+	/>
+}
 
 export default function CardImageRounded({cardID})
 {
-	const [showDefault, setShowDefault] = useState(false)
+	const [visible, setVisible] = useState(false)
+	const [img, setImg] = useState(undefined)
+
+	useEffect( () => {
+		getImg(`https://images.thesupremekingscastle.com/${cardID}.jpg`).then(img => setImg(img))
+	}, [])
+
 
 	return(
-		<div style={{margin: 'auto', marginBottom: '.5rem', width: '85%'}} >
-			<div
-				style={{ borderRadius: '50%', overflow: 'hidden', width: '100%',  height: '0', paddingBottom: '100%' }} >
-					<img
-						alt={`Default Image`}
-						src={`${process.env.PUBLIC_URL}/Img/card_back_square.jpg`}
-						style={(showDefault)? {  width: '100%', objectFit: 'cover' } : {display: 'none'}}
-					/>
-
-					<img
-						alt={`Card Having ID ${cardID}`}
-						src={`https://images.thesupremekingscastle.com/${cardID}.jpg`}
-						style={{  width: '100%', objectFit: 'cover' }}
-						onError={ () => {
-							setShowDefault(true)
-						}}
-					/>
+      <VizSensor
+			partialVisibility
+			offset={{bottom:-5000, top: -10000}}
+			onChange={(isVisible) => {
+				setVisible(isVisible)
+			}} >
+			<div style={{margin: 'auto', marginBottom: '.5rem', width: '85%'}} >
+				<div
+					style={{ borderRadius: '50%', overflow: 'hidden', width: '100%',  height: '0', paddingBottom: '100%' }} >
+					{(visible)? img : undefined
+					}
+				</div>
 			</div>
-		</div>
+
+		</VizSensor>
 	)
 }
