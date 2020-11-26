0104f7d1299e2e794a7387c8833dd5ad135ee19b
Javi Gomez <rtomyj@gmail.com>
2020-11-25 19:10:34 -0600

    Using new CardInformation component

--- src/components/card/CardInformation.js
+++ src/components/card/CardInformation.js
@@ -1,32 +1,23 @@
-import React, { useState, useEffect, lazy, Suspense, memo } from 'react'
-import { Grid, Box, Chip, Typography } from '@material-ui/core'
-import withWidth from '@material-ui/core/withWidth'
+import React, { useState, useEffect, lazy, Suspense, useMemo } from 'react'
+import { Grid, Chip } from '@material-ui/core'
 import {Helmet} from 'react-helmet'
 
-import cardStyles from './YGOCardStyles'
 import { handleFetch } from '../../helper/FetchHandler'
 import NAME_maps_ENDPOINT from '../../helper/YgoApiEndpoints'
 import { MainContentContainer } from '../MainContent'
 
-import { Skeleton } from '@material-ui/lab'
-
 import OneThirdTwoThirdsGrid from '../grid/OneThirdTwoThirdsGrid'
 
-import {StickyBox} from '../util/StyledContainers'
-
 const Breadcrumb = lazy( () => import('../Breadcrumb') )
-
 const CardInformationSection = lazy( () => import('./CardInformationSection') )
-const CardImageRounded = lazy( () => import('./CardImageRounded') )
-const YGOCard = lazy( () => import('./YGOCard') )
-
+const CardData = lazy( () => import('./CardData') )
 const Footer = lazy( () => import('../Footer') )
 
 
 const Card = ( { match, history } ) =>
 {
 	const [isLoading, setIsLoading] = useState(true)
-	const cardId = match.params.cardId
+	const cardID = match.params.cardId
 
 	const [cardName, setCardName] = useState(undefined)
 	const [cardColor, setCardColor] = useState(undefined)
@@ -45,10 +36,8 @@ const Card = ( { match, history } ) =>
 
 	const [dynamicCrumbs, setDynamicCrumbs] = useState(['Home', 'Card Browse', ''])
 
-
-	useEffect( () => {
-
-		handleFetch(`${NAME_maps_ENDPOINT['cardInstanceUrl']}${cardId}?allInfo=true`, history, (json) => {
+	const helmetData = useMemo( () => {
+		handleFetch(`${NAME_maps_ENDPOINT['cardInstanceUrl']}${cardID}?allInfo=true`, history, (json) => {
 			setCardName(json.cardName)
 			setCardColor(json.cardColor)
 			setCardEffect(json.cardEffect)
@@ -65,7 +54,20 @@ const Card = ( { match, history } ) =>
 			setDynamicCrumbs(crumbs)
 			setIsLoading(false)
 		})
+
+		return (
+		<Helmet>
+			<title>SKC - Card: {cardID}</title>
+			<meta
+				name={`SKC - Card: ${cardID}`}
+				content={`Information for YuGiOh card ${cardName} such as ban lists it was in, products it can be found in, effect/stats, etc.`}
+				/>
+			<meta name="keywords" content={`YuGiOh, The Supreme Kings Castle, card, ${cardName}, ${cardID}, ${cardColor}`} />
+		</Helmet>
+		)
+
 	}, [])
+
 
 
 	useEffect( () => {
@@ -99,89 +101,57 @@ const Card = ( { match, history } ) =>
 
 	return (
 		<MainContentContainer style={{ paddingLeft: '0rem', paddingRight: '0rem', paddingBottom: '0rem' }}  >
-			<Helmet>
-				<title>SKC - Card: {cardId}</title>
-				<meta
-					name={`SKC - Card: ${cardId}`}
-					content={`Information for YuGiOh card ${cardName} such as ban lists it was in, products it can be found in, effect/stats, etc.`}
-					/>
-				<meta name="keywords" content={`YuGiOh, The Supreme Kings Castle, card, ${cardName}, ${cardId}, ${cardColor}`} />
-			</Helmet>
-
+			{helmetData}
 			<Suspense>
 				<Breadcrumb crumbs={ dynamicCrumbs } />
 			</Suspense>
 
 			<OneThirdTwoThirdsGrid
 				oneThirdComponent={
-					<StickyBox>
-
-						<Typography
-							variant='h4'
-							align='center'
-							style={{marginBottom: '2rem'}} >
-							Card Information
-						</Typography>
-
-						<Suspense>
-							<CardImageRounded
-								cardID={cardId}
-								/>
-						</Suspense>
-
-						<Suspense fallback={<Skeleton width={'100%'} height={150} />} >
-							<YGOCard
-								isNew={ false }
-								cardName={cardName}
-								cardColor={cardColor}
-								cardEffect={cardEffect}
-								cardAttribute={cardAttribute}
-								monsterType={monsterType}
-								monsterAtk={monsterAtk}
-								monsterDef={monsterDef}
-								monsterAssociation={monsterAssociation}
-								cardStyles={ cardStyles }
-								cardID={cardId}
-								fullDetails={ true }
-								isLoading={ isLoading }
-								/>
-						</Suspense>
-					</StickyBox>
+					<CardData
+						cardName={cardName}
+						cardColor={cardColor}
+						cardEffect={cardEffect}
+						cardAttribute={cardAttribute}
+						monsterType={monsterType}
+						monsterAtk={monsterAtk}
+						monsterDef={monsterDef}
+						monsterAssociation={monsterAssociation}
+						cardID={cardID}
+						isLoading={isLoading}
+					/>
 				}
 				twoThirdComponent={
 					<Grid container >
 						<Grid item xs={12} sm={12} md={12} lg={6} xl={6}  style={ { display: 'inline-grid', padding: '.8rem' } } >
-							<Suspense fallback={undefined}>
-								<CardInformationSection
-									isLoading={isLoading}
-									hasInfo={ (productInfo.length === 0)? false : true }
-									infoChips={productInfoChips}
-									headerText={'Product(s)'}
-									noInfoText={'Not Found In Any Product'}
-									background='#a4508b'
-									backgroundImage='linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)'
-								/>
-							</Suspense>
+							{(isLoading)? undefined
+							: <CardInformationSection
+								isLoading={isLoading}
+								hasInfo={ (productInfo.length === 0)? false : true }
+								infoChips={productInfoChips}
+								headerText={'Product(s)'}
+								noInfoText={'Not Found In Any Product'}
+								background='#a4508b'
+								backgroundImage='linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)'
+							/>
+							}
 						</Grid>
 
 						<Grid item xs={12} sm={12} md={12} lg={6} xl={6} style={ { display: 'inline-grid', padding: '.8rem' } } >
-							<Suspense fallback={undefined}>
-								<CardInformationSection
-									isLoading={isLoading}
-									hasInfo={ (banListInfo.length === 0)? false : true }
-									infoChips={banListInfoChips}
-									headerText={'Ban List(s)'}
-									noInfoText={`No Instances of ${cardName} Being Forbidden, Limited, or Semi-Limited`}
-									background='#fc9842'
-									backgroundImage='linear-gradient(315deg, #fc9842 0%, #fe5f75 74%)'
-								/>
-							</Suspense>
+							{(isLoading)? undefined
+							: <CardInformationSection
+								isLoading={isLoading}
+								hasInfo={ (banListInfo.length === 0)? false : true }
+								infoChips={banListInfoChips}
+								headerText={'Ban List(s)'}
+								noInfoText={`No Instances of ${cardName} Being Forbidden, Limited, or Semi-Limited`}
+								background='#fc9842'
+								backgroundImage='linear-gradient(315deg, #fc9842 0%, #fe5f75 74%)'
+							/>
+							}
 
 						</Grid>
-
-						<Suspense fallback={<Skeleton width={100} height={25} />}>
-							<Footer />
-						</Suspense>
+						<Footer />
 
 					</Grid>
 					}
@@ -192,4 +162,4 @@ const Card = ( { match, history } ) =>
 }
 
 
-export default withWidth()(Card)
+export default Card
