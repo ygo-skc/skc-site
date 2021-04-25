cd02f0076b56d6a53fc359a3d1c9098314706123
Javi Gomez <rtomyj@gmail.com>
2021-04-25 17:43:37 -0500

    Updates to sytles/UI

--- src/components/card/CardImageRounded.js
+++ src/components/card/CardImageRounded.js
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react'
-import { Skeleton } from '@material-ui/lab'
+import '../../css/rounded-img.css'
 import VizSensor from 'react-visibility-sensor'
 
 async function getImg(imgURL)
 {
 	return <img
 		src={imgURL}
-		style={{  width: '100%', height: 'auto', objectFit: 'cover' }}
+		id='rounded-img'
 	/>
 }
 
-export default function CardImageRounded({cardID, timeout=10, defaultVisibility=false})
+
+export default function CardImageRounded({cardID, timeout=0, defaultVisibility=false})
 {
 	const [visible, setVisible] = useState(defaultVisibility)
 	const [img, setImg] = useState(undefined)
@@ -30,10 +31,9 @@ export default function CardImageRounded({cardID, timeout=10, defaultVisibility=
 				if (isVisible !== false)
 				setVisible(isVisible)
 			}} >
-			<div style={{margin: 'auto', marginBottom: '.5rem', width: '96%'}} >
-				<div
-					style={{ borderRadius: '4rem', overflow: 'hidden', width: '100%',  height: '0', paddingBottom: '100%' }} >
-					{(visible)? img : <Skeleton width='100%' height='100%' style={{objectFit: 'cover'}} />
+			<div id='rounded-img-outer-container' >
+				<div id='rounded-img-inner-container' >
+					{(visible)? img : undefined
 					}
 				</div>
 			</div>
