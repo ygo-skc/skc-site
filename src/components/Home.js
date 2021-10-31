ee759cf0571c4ab074465457796e9c07f6bc55b1
Javi <rtomyj@gmail.com>
2021-10-31 14:01:27 -0500

    Updated heart uri

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,7 +35,7 @@ export default function Home({ history }) {
 		})
 
 
-		handleFetch(`${HEART_API_HOST_NAME}/v1/yt/channel/uploads?channelId=UCBZ_1wWyLQI3SV9IgLbyiNQ`, history, json => {
+		handleFetch(`${HEART_API_HOST_NAME}/api/v1/yt/channel/uploads?channelId=UCBZ_1wWyLQI3SV9IgLbyiNQ&key=Myw2gdS73mscn38FEKP4mPguk3QvuUeW`, history, json => {
 			setYoutubeData(json)
 		})
 	}, [history])
