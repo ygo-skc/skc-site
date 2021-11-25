22aaeb8d3646227805fbffb665186ee53ae9dc02
Javi <rtomyj@gmail.com>
2021-11-24 18:05:56 -0600

    Cached last seen message so users dont see notification icon when they already saw notifications.

--- src/components/header-footer/Messages.tsx
+++ src/components/header-footer/Messages.tsx
@@ -8,28 +8,60 @@ import ReactMarkdown from 'react-markdown'
 
 import {HEART_API_HOST_NAME} from '../../helper/DownstreamServices'
 import { handleFetch } from '../../helper/FetchHandler'
-import { getDateString } from '../../helper/Dates'
+import { getDateString, getTimeString } from '../../helper/Dates'
 import { MessageItem } from '../types/HeartApiTypes'
 
 
 export default function Messages() {
-	const [communicationAnchor, setCommunicationAnchor] = useState<HTMLButtonElement | undefined>(undefined)
-	const [communicationList, setCommunicationList] = useState([])
-	const [communicationListSize, setCommunicationListSize] = useState(0)
+	const [messagesAnchor, setMessagesAnchor] = useState<HTMLButtonElement | undefined>(undefined)
+	const [messagesList, setMessagesList] = useState([])
 
-	const isDisplayingNotifications = Boolean(communicationAnchor)
+	const [numMessages, setNumMessages] = useState(0)
+	const [numNewMessages, setNumNewMessages] = useState(0)
+
+	const [newestMessageSeen, setNewestMessageSeen] = useState<string>('')
+
+	const isDisplayingNotifications = Boolean(messagesAnchor)
+
 
 	useEffect(() => {
 		handleFetch(`${HEART_API_HOST_NAME}/api/v1/message?service=skc&tags=general`, json => {
-			const totalCommunicationItems = json.messages.length
-			setCommunicationListSize(totalCommunicationItems)
+			const totalMessages = json.messages.length
+			setNumMessages(totalMessages)
 
-			setCommunicationList(
+
+			let findNumNewMessages = false
+			let _numNewMessages = 0
+			const previousNewestMessageTimeStamp = localStorage.getItem('previousNewestMessage') as string
+			const previousNewestMessageDate = new Date(previousNewestMessageTimeStamp)
+
+			if (totalMessages > 0) {
+				setNewestMessageSeen(json.messages[0].createdAt)
+
+				if (previousNewestMessageTimeStamp !== json.messages[0].createdAt) {
+					findNumNewMessages = true
+				}
+			}
+
+			setMessagesList(
 				json.messages.map((message: MessageItem, index: number) => {
+					const creationDate = new Date(message.createdAt)
+
+					if (findNumNewMessages) {
+						if (previousNewestMessageDate >= creationDate ) {
+							findNumNewMessages = false
+						} else {
+							_numNewMessages++
+						}
+					}
+
+
 					return(
 						<div>
 							<Typography className='communication-message-header' variant='h5'>{message.title}</Typography>
-							<Typography className='communication-message-sub-header' variant='subtitle2' >{getDateString(new Date(message.createdAt))}</Typography>
+							<Typography className='communication-message-sub-header' variant='subtitle2' >
+								{getDateString(creationDate)} {getTimeString(creationDate)}
+							</Typography>
 							<Typography className='communication-message-body' variant='body1' >
 								<ReactMarkdown children={`${message.content}`} />
 							</Typography>
@@ -37,12 +69,15 @@ export default function Messages() {
 								message.tags.map((tag: string) => <Chip className='communication-message-tag' label={tag} />)
 							}
 							{
-								(index === totalCommunicationItems - 1)? undefined: <Divider className='communication-divider' />
+								(index === totalMessages - 1)? undefined: <Divider className='communication-divider' />
 							}
 						</div>
 					)
 				})
 			)
+
+
+			setNumNewMessages(_numNewMessages)
 		})
 	}, [])
 
@@ -51,12 +86,12 @@ export default function Messages() {
 		<div>
 			<Badge
 				className='communication-message-badge'
-				badgeContent={communicationListSize}
+				badgeContent={numNewMessages}
 				variant='standard'
 				color='error' >
 				<IconButton
 					className='styled-icon-button'
-					onClick={(event: React.MouseEvent<HTMLButtonElement>) => {setCommunicationAnchor(event.currentTarget)}}
+					onClick={(event: React.MouseEvent<HTMLButtonElement>) => {setMessagesAnchor(event.currentTarget)}}
 					aria-label="show 17 new notifications"
 					color="inherit">
 						<NotificationsIcon />
@@ -66,18 +101,23 @@ export default function Messages() {
 			<Popover
 				id={(isDisplayingNotifications)? 'notification-popover' : undefined}
 				open={isDisplayingNotifications}
-				anchorEl={communicationAnchor}
-				onClose={() => setCommunicationAnchor(undefined)}
+				anchorEl={messagesAnchor}
+				onClose={() => {
+					setNumNewMessages(0)
+					setMessagesAnchor(undefined)
+					console.log(newestMessageSeen)
+					localStorage.setItem('previousNewestMessage', newestMessageSeen)
+				}}
 				anchorOrigin={{
 					vertical: 'bottom',
 					horizontal: 'left',
 				}}>
 				<div className='communication-popper-container' >
 					<Typography className='communication-message-body' variant='h4' >
-						Messages ({communicationListSize})
+						Messages ({numMessages})
 					</Typography>
 
-					{communicationList}
+					{messagesList}
 				</div>
 			</Popover>
 		</div>
