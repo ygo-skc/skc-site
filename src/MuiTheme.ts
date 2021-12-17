be08300d579cb2fa9e04299f7fb3a8d4c467f73e
Javi <rtomyj@gmail.com>
2021-12-16 20:51:53 -0600

    Moved Mui theme to own file. Migrated index and Mui Theme file to ts

--- src/MuiTheme.ts
+++ src/MuiTheme.ts
@@ -1,31 +1,26 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 
-import Footer from './components/header-footer/Footer'
-import NavigationBar from './components/header-footer/NavigationBar'
-import Routes from './Routes.tsx'
-
-
-const theme = createTheme({
+const MuiTheme = createTheme({
 	palette: {
 		primary: {
 			main: '#53539e',
-			contrastDefaultColor: 'light'
+			// contrastDefaultColor: 'light',
 		},
 		secondary: {
 			main: '#ff8f44',
-			contrastDefaultColor: 'light'
+			// contrastDefaultColor: 'light',
 		},
 		error: {
 			main: '#f50057',
-		}
+		},
 	},
 	components: {
 		MuiDivider: {
-			root: {
-				marginBottom: '.5rem',
-			}
+			styleOverrides: {
+				root: {
+					marginBottom: '.5rem',
+				},
+			},
 		},
 		MuiChip: {
 			styleOverrides: {
@@ -34,41 +29,41 @@ const theme = createTheme({
 					marginRight: '.55rem',
 					marginTop: '.5rem',
 					backgroundColor: 'rgba(0, 0, 0, .23)',
-					color: 'white'
-				}
-				, label: {
+					color: 'white',
+				},
+				label: {
 					fontWeight: 600,
-					fontSize: '.98rem'
-				}
-				, clickable: {
+					fontSize: '.98rem',
+				},
+				clickable: {
 					'&:hover': {
-						backgroundColor: 'rgba(0, 0, 0, .5)'
-					}
-					, '&:focus': {
-						backgroundColor: 'rgba(0, 0, 0, .6)'
-					}
-				}
-			}
+						backgroundColor: 'rgba(0, 0, 0, .5)',
+					},
+					'&:focus': {
+						backgroundColor: 'rgba(0, 0, 0, .6)',
+					},
+				},
+			},
 		},
 		MuiBadge: {
 			styleOverrides: {
 				badge: {
-					fontSize: '1.05rem'
-					, paddingLeft: '.8rem'
-					, paddingRight: '.8rem'
-					, paddingTop: '.4rem'
-					, paddingBottom: '.4rem'
-					, fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
-				}
-			}
+					fontSize: '1.05rem',
+					paddingLeft: '.8rem',
+					paddingRight: '.8rem',
+					paddingTop: '.4rem',
+					paddingBottom: '.4rem',
+					fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
+				},
+			},
 		},
 		MuiPopover: {
 			styleOverrides: {
 				paper: {
-					background: 'rgba(255, 255, 255, .6)'
-					, backdropFilter: 'blur(60px)'
-				}
-			}
+					background: 'rgba(255, 255, 255, .6)',
+					backdropFilter: 'blur(60px)',
+				},
+			},
 		},
 		MuiAutocomplete: {
 			styleOverrides: {
@@ -77,14 +72,14 @@ const theme = createTheme({
 					borderRadius: '1.5rem',
 					border: '3px rgba(135, 120, 229, .7)',
 					borderStyle: 'solid',
-					boxShadow: 'rgba(0,0,0,0.12) 0px 1px 6px'
+					boxShadow: 'rgba(0,0,0,0.12) 0px 1px 6px',
 				},
 				option: {
 					paddingLeft: '0rem',
 					paddingReft: '0rem',
-					paddingBottom: '.4rem'
-				}
-			}
+					paddingBottom: '.4rem',
+				},
+			},
 		},
 		MuiTooltip: {
 			styleOverrides: {
@@ -94,43 +89,43 @@ const theme = createTheme({
 					lineHeight: '1.3rem',
 					fontSize: '.88rem',
 					backgroundColor: 'black',
-					padding: '.75rem'
+					padding: '.75rem',
 				},
 				arrow: {
 					backgroundColor: 'black',
-					color: '#ddd'
-				}
-			}
-		}
-		, MuiTableCell: {
+					color: '#ddd',
+				},
+			},
+		},
+		MuiTableCell: {
 			styleOverrides: {
 				stickyHeader: {
 					fontWeight: 800,
-					fontSize: '.94rem'
+					fontSize: '.94rem',
 				},
 				head: {
 					fontWeight: 800,
-					fontSize: '.94rem'
+					fontSize: '.94rem',
 				},
 				body: {
 					fontWeight: 600,
-					fontSize: '.94rem'
+					fontSize: '.94rem',
 				},
 				root: {
 					fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
 					padding: '.4rem',
-					'border-bottom': '0rem'
-				}
-			}
+					'border-bottom': '0rem',
+				},
+			},
 		},
 		MuiPaper: {
 			styleOverrides: {
 				elevation1: {
-					boxShadow: 'rgba(0,0,0,0.12) 0px 1px 6px'
-					, border: '1px solid rgb(221, 221, 221)'
-				}
-			}
-		}
+					boxShadow: 'rgba(0,0,0,0.12) 0px 1px 6px',
+					border: '1px solid rgb(221, 221, 221)',
+				},
+			},
+		},
 	},
 	typography: {
 		button: {
@@ -173,7 +168,7 @@ const theme = createTheme({
 			lineHeight: '1.45rem',
 			marginBottom: '1rem',
 			color: '#626262',
-			fontSize: '1.3rem'
+			fontSize: '1.3rem',
 		},
 		h6: {
 			fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
@@ -213,22 +208,7 @@ const theme = createTheme({
 			color: '#444',
 			fontSize: '.9rem',
 		},
-	}
+	},
 })
 
-render(
-	<ThemeProvider
-		theme={ theme } >
-		<div className='content' >
-			<NavigationBar />
-			<div className='routes' >
-				<div className='routes-wrapper' >
-					<Routes />
-				</div>
-			</div>
-		</div>
-		<Footer />
-	</ThemeProvider>
-	,
-	document.getElementById('root')
-)
+export default MuiTheme
