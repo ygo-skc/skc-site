f1764ea325c3418a6d0e700846aa32b761960fd3
Javi <rtomyj@gmail.com>
2021-12-26 14:58:16 -0600

    Even more JavaScript to TypeScript

---
+++ src/components/banlist/BanListSection.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, FC } from 'react'
+import CardDisplayGrid from '../util/grid/CardDisplayGrid'
+import { Hint } from '../util/Hints'
+
+type _BanListSection = {
+	sectionExplanation: string
+	cards: SKCCard[]
+	isDataLoaded: boolean
+}
+
+const BanListSection: FC<_BanListSection> = ({ sectionExplanation, cards, isDataLoaded }) => {
+	const [cardTypeContentGrid, setCardTypeContentGrid] = useState<JSX.Element>()
+
+	useEffect(() => {
+		if (isDataLoaded) {
+			setCardTypeContentGrid(
+				<CardDisplayGrid
+					cardJsonResults={cards}
+					numResultsDisplayed={cards.length}
+					numItemsToLoadWhenNeeded={cards.length}
+					loadMoreCallback={undefined}
+					isLoadMoreOptionVisible={false}
+					numResults={cards.length}
+					isDataLoaded={isDataLoaded}
+				/>
+			)
+		} else setCardTypeContentGrid(undefined)
+	}, [isDataLoaded])
+
+	return (
+		<div>
+			<Hint variant='subtitle1'>{sectionExplanation}</Hint>
+			{cardTypeContentGrid}
+		</div>
+	)
+}
+
+export default BanListSection
