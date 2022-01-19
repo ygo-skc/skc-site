a420507e01435f5de3336d22f7699575da1af292
Javi <rtomyj@gmail.com>
2022-01-18 21:47:32 -0600

    Updated my info

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -76,30 +76,41 @@ const About: FunctionComponent = () => {
 					</InfoPaper2Header>
 
 					<div style={{ padding: '1.4rem' }}>
-						<Typography variant='h6'>Background</Typography>
+						<Typography variant='h6'>My Background</Typography>
 						<ParagraphTypography variant='body1'>
-							I started playing Yugioh when I was about 8 years old. I played at some local tourneys, but I wasn't exceptional by any means. I stopped playing when I got to high
-							school, but I still collected cards for fun. My collecting then stopped when I got to college since it was hard convincing myself a pack of cards was more important
-							than food/gas. Also, being a CS major and a Math minor with a full time job - hobbies were not really a priority.
+							I started playing Yu-Gi-Oh! when I was about 8 years old. I would do some tournaments when I was in middle school and always found it fun to open packs. Starting high
+							school I was less interested in tournaments and more into collecting, but around my Sophomore/Junior year It was hard to be into the hobby. I got back into it
+							slightly in my early college days when i could afford the hobby a bit more, but ultimately I perceived it as a waste of time/money as I had little of either and
+							looking back, I was right.
 						</ParagraphTypography>
 						<br />
 						<ParagraphTypography>
-							I graduated sometime in 2018 and got a decent job in my field. Through it, I got more time and money for Yugioh again! I play HERO's and Neo Spacians primarily. Other
-							archetypes I like are; Sacred Beasts, Cyber Dragons, and Toons. I primarily collect cards and play Duel Links. Tournaments have been on my mind as well.
+							I graduated college some time in 2018 and got a job in my field. Through it, I got more money and slightly more time! In my field there is always something new to
+							learn. Learning on the job is a hinderance in most cases. I figured I'd learn new technologies by building this web site and some web API's to support the backend. I
+							was right about this too since everything I'm learning to build this site has helped me stay ahead of my colleagues.
 						</ParagraphTypography>
 						<br />
-						<Typography variant='h6'>Purpose</Typography>
+						<Typography variant='h6'>Purpose Of This Site</Typography>
 						<ParagraphTypography>
-							This website was created for two reasons. The first and most important was that I needed to learn technologies I wasn't exposed to (primarily Spring) that I needed to
-							know for my job. In case you are wondering, it did help me become adept and am able to do my job faster than most and with ease. Secondly, I thought I could do a
-							better job displaying content than Konami. This website began as a place to display info about the current and previous ban lists. However, I wanted to keep going and
-							built a full blown Database for Yu-Gi-Oh content.
+							In short, I thought I could do a better job displaying content than Konami and other fan made sites. Take a look at the existing sites and come back and tell me mine
+							isn't faster, cleaner and with less (no) invasive ads. This website began as a place to display info about the current and previous ban lists. However, I wanted to
+							keep going and built a full blown Database for Yu-Gi-Oh content. There is some stuff missing and my site is far from perfect, but It will be one of the best sites for
+							Yu-Gi-Oh content one day.
 						</ParagraphTypography>
 						<br />
 						<Typography variant='h6'>Future Features</Typography>
 						<ParagraphTypography>
 							I have some more plans for the website so please stay tuned. Something I wanted to start learning was Machine Learning / AI. The first step I see myself taking is
 							creating a robust suggestion engine that uses card information (parsing text) to help Duelists find related cards easier. I haven't seen a good engine yet...
+						</ParagraphTypography>
+						<br />
+						<ParagraphTypography>
+							I have also recently started selling cards online. I don't like the models sites like TCGPlayer or EBay have for sellers (or the UI they have for that matter) and
+							want to have my own site where I can keep all the profit I make. Essentially, this site will be a hub for my Yu-Gi-Oh! related endeavors (see the{' '}
+							<Link color='secondary' className='link' href='/'>
+								home page
+							</Link>{' '}
+							for what I mean) and, one day, the best Yu-Gi-Oh! DB!
 						</ParagraphTypography>
 					</div>
 				</InfoPaper2>
