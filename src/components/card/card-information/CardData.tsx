22353ba7cbc08237eeb8c1ab95528396f8e72b0c
Javi <rtomyj@gmail.com>
2022-01-20 20:53:38 -0600

    Encapsulated Card info with Section component

--- src/components/card/card-information/CardData.tsx
+++ src/components/card/card-information/CardData.tsx
@@ -1,30 +1,35 @@
 import { FC, memo } from 'react'
 
-import { Box } from '@mui/material'
 import CardImageRounded from '../CardImageRounded'
 
 import YGOCard from '../YGOCard'
+import Section from '../../util/Section'
 
 const CardData: FC<_CardData> = memo(
 	({ cardID, cardName, cardColor, cardEffect, cardAttribute, monsterType, monsterAtk, monsterDef, monsterAssociation, isLoading, cardImg }) => {
 		return (
-			<Box className='sticky one-third-two-thirds-container'>
-				<CardImageRounded cardImg={cardImg.src} defaultVisibility={true} />
+			<Section
+				sectionName='Information'
+				sectionContent={
+					<div className='sticky section-content'>
+						<CardImageRounded cardImg={cardImg.src} defaultVisibility={true} />
 
-				<YGOCard
-					cardName={cardName}
-					cardColor={cardColor}
-					cardEffect={cardEffect}
-					cardAttribute={cardAttribute}
-					monsterType={monsterType}
-					monsterAtk={monsterAtk}
-					monsterDef={monsterDef}
-					monsterAssociation={monsterAssociation}
-					cardID={cardID}
-					fullDetails={true}
-					isLoading={isLoading}
-				/>
-			</Box>
+						<YGOCard
+							cardName={cardName}
+							cardColor={cardColor}
+							cardEffect={cardEffect}
+							cardAttribute={cardAttribute}
+							monsterType={monsterType}
+							monsterAtk={monsterAtk}
+							monsterDef={monsterDef}
+							monsterAssociation={monsterAssociation}
+							cardID={cardID}
+							fullDetails={true}
+							isLoading={isLoading}
+						/>
+					</div>
+				}
+			/>
 		)
 	},
 	(prevProps, newProps) => {
--- src/components/util/Section.tsx
+++ src/components/util/Section.tsx
@@ -12,7 +12,7 @@ const Section: FC<SectionType> = ({ sectionName, sectionContent }) => {
 	return (
 		<div className='section-parent heavy-shadow'>
 			<div className='section-header-container'>
-				<Typography variant='h1' className='section-header-text'>
+				<Typography variant='h1' align='center' className='section-header-text'>
 					{sectionName}
 				</Typography>
 			</div>
