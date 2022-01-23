b03084844c594c7664ac548bc8eb4e74e865e77e
Javi <rtomyj@gmail.com>
2022-01-23 11:16:58 -0600

    Decoupled components. Replacing existing containers with Section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,16 +1,12 @@
-import { FunctionComponent, lazy } from 'react'
+import { FunctionComponent } from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Typography, Paper, Grid, Link } from '@mui/material'
+import { Typography, Paper, Link } from '@mui/material'
 import styled from 'styled-components'
 
 import Breadcrumb from '../header-footer/Breadcrumb'
-
-const AboutInfoCard = lazy(() => import('./AboutInfoCard'))
-
-const CenteredImage = styled.div`
-	text-align: center;
-`
+import Section from '../util/Section'
+import Overview from './Overview'
 
 const InfoPaper2 = styled(Paper)`
 	&& {
@@ -29,21 +25,6 @@ const InfoPaper2Header = styled.div`
 		background: #ffa366;
 		border-top-left-radius: 1.2rem;
 		border-top-right-radius: 1.2rem;
-	}
-`
-
-const InfoContainer = styled(InfoPaper2)`
-	&& {
-		max-width: 100%;
-		background-color: #3f1286;
-		background: radial-gradient(89.53% 145.96% at 60% 100.79%, #461495 0, #5418b3 17.58%, #461495 50.31%, #3f1286 97.03%);
-		margin-top: 2rem;
-	}
-`
-
-const InfoContainerHeader = styled(InfoPaper2Header)`
-	&& {
-		background: rgba(0, 0, 0, 0.4);
 	}
 `
 
@@ -114,158 +95,9 @@ const About: FunctionComponent = () => {
 						</ParagraphTypography>
 					</div>
 				</InfoPaper2>
-
-				<InfoContainer>
-					<InfoContainerHeader>
-						<Typography variant='h1' style={{ color: 'white' }}>
-							Information
-						</Typography>
-						<Typography variant='h3' style={{ color: 'white' }}>
-							SKC API, Donate, Contact Info, And More!
-						</Typography>
-					</InfoContainerHeader>
-
-					<div style={{ padding: '1.4rem' }}>
-						<Grid container spacing={2}>
-							<Grid style={{ display: 'inline-grid' }} item xs={12} sm={12} md={6} lg={4} xl={4}>
-								<AboutInfoCard
-									title='SKC API'
-									subtitle='The Backend'
-									body={
-										<div>
-											<Typography variant='body1'>
-												A new API was created in the process of creating this site. Though there are other API's available, my purpose was in creating my own to learn a new stack.
-											</Typography>
-											<br />
-											<Typography variant='body1'>
-												This is good for other developers. It means you have another <strong>[FREE]</strong> choice when you want to learn about API's or need an API for a small
-												project.
-											</Typography>
-											<br />
-											<Typography variant='body1'>
-												You can start using it by looking at the{' '}
-												<Link className='link' color='secondary' href={`${process.env.REACT_APP_API_HOST}/api/v1/swagger-ui/index.html`}>
-													documentation
-												</Link>
-												. As of right now, the only permissible uses are for education (learning about REST, needing data for a consumer, etc). If you have question on usage please
-												email me.
-											</Typography>
-										</div>
-									}
-									imgPath='backend.png'
-									imgLink={`${process.env.REACT_APP_API_HOST}/api/v1/swagger-ui/index.html`}
-								/>
-							</Grid>
-
-							<Grid style={{ display: 'inline-grid' }} item xs={12} sm={12} md={6} lg={4} xl={4}>
-								<AboutInfoCard
-									title='Contributions'
-									subtitle='Donations To Website'
-									body={
-										<div>
-											<Typography variant='body1'>
-												As you can tell, this site has no ads and no trackers trying to get information about you. This has two intended effects:
-												<ol>
-													<li>The interface is fast, clean and intuitive.</li>
-													<li>Big corporations don't get browsing data about you.</li>
-												</ol>
-											</Typography>
-
-											<Typography variant='body1'>
-												To help maintain this web page, you can donate using one of the options below.
-												<ul>
-													<li style={{ wordBreak: 'break-all' }}>
-														<strong>BTC (Bitcoin) Wallet:</strong> 3DbG1525F4HMf9Qttj5waDxJv747JrBh4g
-													</li>
-													<li style={{ wordBreak: 'break-all' }}>
-														<strong>ZEC (Zcash) Wallet:</strong> t1UdXZkAUUNY35Kh2FiRoRk29Xu8SEYsXxS
-													</li>
-													<li>
-														<Link className='link' color='secondary' href='https://www.paypal.com/donate?hosted_button_id=S3MFF6AVW662U'>
-															PayPal
-														</Link>
-													</li>
-												</ul>
-											</Typography>
-
-											<CenteredImage>
-												<form action='https://www.paypal.com/donate' method='post' target='_top'>
-													<input type='hidden' name='hosted_button_id' value='S3MFF6AVW662U' />
-													<input
-														type='image'
-														src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
-														name='submit'
-														title='PayPal - The safer, easier way to pay online!'
-														alt='Donate with PayPal button'
-													/>
-													<img alt='' src='https://www.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1' />
-												</form>
-											</CenteredImage>
-										</div>
-									}
-									imgPath='monetary_contribution.png'
-									imgLink=''
-								/>
-							</Grid>
-
-							<Grid style={{ display: 'inline-grid' }} item xs={12} sm={12} md={6} lg={4} xl={4}>
-								<AboutInfoCard
-									title='Other Information'
-									subtitle='Contact & Other Stuff'
-									body={
-										<div>
-											<Typography variant='h6'>Contact</Typography>
-											<ParagraphTypography variant='body1'>
-												If you have questions or want to submit a bug/feature request you can always{' '}
-												<Link className='link' color='secondary' href='mailto:thesupremeking25@gmail.com'>
-													email me
-												</Link>
-												. Otherwise, feel free to follow me on{' '}
-												<Link className='link' color='secondary' href='https://twitter.com/SupremeKing93'>
-													twitter
-												</Link>
-												. Though I don't really do social media, I will try my best to post updates there.
-											</ParagraphTypography>
-
-											<br />
-											<Typography variant='h6'>Privacy</Typography>
-											<ParagraphTypography variant='body1'>
-												Though there are no trackers on this site, every request sent to the API has the IP address of the requester. This is strictly to fight DDOS attacks by
-												blacklisting IPs based on attacks. Regular user IPs are not stored in a database and are only written to a log accessible only by me.
-											</ParagraphTypography>
-										</div>
-									}
-									imgPath='information.png'
-									imgLink=''
-								/>
-							</Grid>
-
-							<Grid style={{ display: 'inline-grid' }} item xs={12} sm={12} md={6} lg={4} xl={4}>
-								<AboutInfoCard
-									title='Watch Yu-Gi-Oh! Related Content'
-									subtitle='Subscribe On YouTube'
-									body={
-										<Typography variant='body1'>
-											To help keep the site up (and in no ways an excuse to buy more cards), I started an unboxing/pack opening channel. I feel that I could give you, the
-											consumers, more content. While also making money to support this project.
-											<br />
-											<br />
-											The channel is pretty laid back and I won't be screaming/freaking out every time I get a rare card. In other words, It is an alternative to the fake or over
-											the top reaction based Yugioh openings. Feel free to{' '}
-											<Link className='link' color='secondary' href='https://www.youtube.com/channel/UCBZ_1wWyLQI3SV9IgLbyiNQ/videos'>
-												watch/subscribe
-											</Link>
-											!
-										</Typography>
-									}
-									imgPath='yt_channel_icon.jpeg'
-									imgLink='https://www.youtube.com/channel/UCBZ_1wWyLQI3SV9IgLbyiNQ/videos'
-								/>
-							</Grid>
-						</Grid>
-					</div>
-				</InfoContainer>
 			</div>
+
+			<Section sectionName='Information' sectionContent={<Overview />} />
 		</div>
 	)
 }
