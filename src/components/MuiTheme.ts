e95431c2720d64e429abb160b110bd8565482044
Javi <rtomyj@gmail.com>
2022-04-07 21:06:49 -0500

    Fixed warning

--- src/components/MuiTheme.ts
+++ src/components/MuiTheme.ts
@@ -118,7 +118,7 @@ const MuiTheme = createTheme({
 				root: {
 					fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
 					padding: '.4rem',
-					'border-bottom': '0rem',
+					borderBottom: '0rem',
 				},
 			},
 		},
