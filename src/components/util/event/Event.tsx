8ac72a5624608af6ed0ee239c52b75caea380acd
Javi <rtomyj@gmail.com>
2022-04-17 22:19:32 -0500

    Updated styles

--- src/components/util/event/Event.tsx
+++ src/components/util/event/Event.tsx
@@ -26,8 +26,8 @@ const Event = () => {
 			return (
 				<div
 					style={{
-						padding: '1rem',
-						width: '25rem',
+						padding: '.5rem',
+						width: '24rem',
 						borderRadius: '1.5rem',
 						background: 'white',
 						marginRight: '1rem',
@@ -37,13 +37,13 @@ const Event = () => {
 				>
 					<DateGlance date={new Date(event.eventDate)} />
 
-					<Typography style={{ maxLines: '2', height: '3rem' }} align='center' variant='h5'>
+					<Typography style={{ maxLines: '2', height: '3rem', fontWeight: '800', marginBottom: '0' }} align='center' variant='h5'>
 						{event.name}
 					</Typography>
-					<Typography variant='body2'>
+					<Typography style={{ height: '10rem', overflowY: 'auto' }} variant='body1'>
 						<ReactMarkdown children={`${event.notes}`} />
 					</Typography>
-					<Typography variant='subtitle1' align='right'>
+					<Typography style={{ marginBottom: '0' }} variant='subtitle1' align='right'>
 						<Link color='secondary' className='link' href={event.url}>
 							{'More Info >'}
 						</Link>
