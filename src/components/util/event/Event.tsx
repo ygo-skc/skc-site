f8e870ff9c3de6fc555e17246b2b665a20770b7f
Javi <rtomyj@gmail.com>
2022-04-17 22:23:09 -0500

    Separated components for simplicity

--- src/components/util/event/Event.tsx
+++ src/components/util/event/Event.tsx
@@ -1,9 +1,8 @@
-import { Link, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
-import ReactMarkdown from 'react-markdown'
 import DownstreamServices from '../../../helper/DownstreamServices'
 import Fetch from '../../../helper/FetchHandler'
-import DateGlance from '../DateGlance'
+import EventItem from './EventItem'
 
 const Event = () => {
 	const [events, setEvents] = useState<HeartApiEventItem[]>([])
@@ -23,33 +22,7 @@ const Event = () => {
 
 	useEffect(() => {
 		const eUI = events.map((event: HeartApiEventItem) => {
-			return (
-				<div
-					style={{
-						padding: '.5rem',
-						width: '24rem',
-						borderRadius: '1.5rem',
-						background: 'white',
-						marginRight: '1rem',
-						marginBottom: '1rem',
-						border: '3px solid #eb608f',
-					}}
-				>
-					<DateGlance date={new Date(event.eventDate)} />
-
-					<Typography style={{ maxLines: '2', height: '3rem', fontWeight: '800', marginBottom: '0' }} align='center' variant='h5'>
-						{event.name}
-					</Typography>
-					<Typography style={{ height: '10rem', overflowY: 'auto' }} variant='body1'>
-						<ReactMarkdown children={`${event.notes}`} />
-					</Typography>
-					<Typography style={{ marginBottom: '0' }} variant='subtitle1' align='right'>
-						<Link color='secondary' className='link' href={event.url}>
-							{'More Info >'}
-						</Link>
-					</Typography>
-				</div>
-			)
+			return <EventItem event={event} />
 		})
 
 		setEventsUI(eUI)
---
+++ src/components/util/event/EventItem.tsx
@@ -0,0 +1,36 @@
+import { Link, Typography } from '@mui/material'
+import { FC } from 'react'
+import ReactMarkdown from 'react-markdown'
+import DateGlance from '../DateGlance'
+
+const EventItem: FC<{ event: HeartApiEventItem }> = ({ event }) => {
+	return (
+		<div
+			style={{
+				padding: '.5rem',
+				width: '24rem',
+				borderRadius: '1.5rem',
+				background: 'white',
+				marginRight: '1rem',
+				marginBottom: '1rem',
+				border: '3px solid #eb608f',
+			}}
+		>
+			<DateGlance date={new Date(event.eventDate)} />
+
+			<Typography style={{ maxLines: '2', height: '3rem', fontWeight: '800', marginBottom: '0' }} align='center' variant='h5'>
+				{event.name}
+			</Typography>
+			<Typography style={{ height: '10rem', overflowY: 'auto' }} variant='body1'>
+				<ReactMarkdown children={`${event.notes}`} />
+			</Typography>
+			<Typography style={{ marginBottom: '0' }} variant='subtitle1' align='right'>
+				<Link color='secondary' className='link' href={event.url}>
+					{'More Info >'}
+				</Link>
+			</Typography>
+		</div>
+	)
+}
+
+export default EventItem
