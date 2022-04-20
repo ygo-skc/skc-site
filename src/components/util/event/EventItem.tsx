dbce949d0194b03b0d23d121eedd8e546b082da1
Javi <rtomyj@gmail.com>
2022-04-20 14:41:42 -0500

    Updated styles/look

--- src/components/util/event/EventItem.tsx
+++ src/components/util/event/EventItem.tsx
@@ -7,16 +7,19 @@ const EventItem: FC<{ event: HeartApiEventItem }> = ({ event }) => {
 	return (
 		<div className='event-item'>
 			<div className='event-item-header'>
-				<Typography className='event-name' variant='h5'>
+				<Typography className='event-name' variant='h6'>
 					{event.name}
 				</Typography>
 				<DateGlance date={new Date(event.eventDate)} />
 			</div>
 
 			<Typography className='event-notes' variant='body2'>
+				<Typography className='event-notes-header' variant='subtitle1'>
+					Notes
+				</Typography>
 				<ReactMarkdown children={`${event.notes}`} />
 			</Typography>
-			<Typography className='event-url' variant='subtitle1' align='right'>
+			<Typography className='event-url' variant='body1' align='right'>
 				<Link color='secondary' className='link' href={event.url} target='_blank'>
 					{'More Info >'}
 				</Link>
