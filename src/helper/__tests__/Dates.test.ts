405cb7baa4473f3a3f4013b7c4daecbe6b59953c
Javi <rtomyj@gmail.com>
2022-08-09 20:45:05 -0500

    New tests

--- src/helper/__tests__/Dates.test.ts
+++ src/helper/__tests__/Dates.test.ts
@@ -3,6 +3,11 @@ import { Dates } from '../Dates'
 const date = new Date(746322027000)
 const date2 = new Date(680664480000)
 
+test('verify date gets formatted with {Day-Of-Week Month day, year}', () => {
+	expect(Dates.getNonLocalizedDateString(date)).toBe('Wed Aug 25 1993')
+	expect(Dates.getNonLocalizedDateString(date2)).toBe('Sat Jul 27 1991')
+})
+
 test('verify date gets formatted with {month day, year}', () => {
 	expect(Dates.getDateString(date)).toBe('Aug 25, 1993')
 	expect(Dates.getDateString(date2)).toBe('Jul 27, 1991')
@@ -33,4 +38,17 @@ test('verify ban list date gets formatted correctly', () => {
 	expect(Dates.banListDate('2021-02-10')).toBe('Feb 10, 2021')
 	expect(Dates.banListDate('1993-12-31')).toBe('Dec 31, 1993')
 	expect(Dates.banListDate('2001-09-11')).toBe('Sep 11, 2001')
+})
+
+test("verify retrieval of ban list date range doesn't cause issues with undefined input", () => {
+	expect(Dates.getCurrentBanListDate()).toBe('')
+	expect(Dates.getCurrentBanListDate('', undefined)).toBe('')
+})
+
+test('verify retrieval of ban list date range works as intended', () => {
+	const dates = ['2021-01-10', '2021-03-11', '2021-07-01']
+
+	expect(Dates.getCurrentBanListDate('2021-01-10', dates)).toBe('Jan 10, 2021 - Present')
+	expect(Dates.getCurrentBanListDate('2021-03-11', dates)).toBe('Mar 11, 2021 - Jan 10, 2021')
+	expect(Dates.getCurrentBanListDate('2021-07-01', dates)).toBe('Jul 1, 2021 - Mar 11, 2021')
 })
