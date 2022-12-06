0247507ddaaba463055d2dbf73f1e72d823314c7
Javi <rtomyj@gmail.com>
2022-12-05 23:16:37 -0600

    Added startTransition

--- src/components/card/CardImageRounded.tsx
+++ src/components/card/CardImageRounded.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, startTransition, useState } from 'react'
 import '../../css/rounded-img.css'
 import VizSensor from 'react-visibility-sensor'
 
@@ -15,7 +15,9 @@ const CardImageRounded: FC<_CardImageRounded> = ({ cardImg, defaultVisibility = 
 			partialVisibility
 			offset={{ bottom: -350, top: -200 }}
 			onChange={(isVisible: boolean) => {
-				if (isVisible !== false) setVisible(isVisible)
+				startTransition(() => {
+					if (isVisible !== false) setVisible(isVisible)
+				})
 			}}
 		>
 			<div id='rounded-img-outer-container'>
