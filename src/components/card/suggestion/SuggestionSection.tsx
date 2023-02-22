1fa12c5127df9b475461dffc90ae3ca3ecfdb84b
Javi <rtomyj@gmail.com>
2023-02-21 19:14:26 -0600

    Fixing issue

--- src/components/card/suggestion/SuggestionSection.tsx
+++ src/components/card/suggestion/SuggestionSection.tsx
@@ -9,13 +9,14 @@ type _SuggestionSection = {
 const SuggestionSection: FC<_SuggestionSection> = ({ suggestions, sectionName }) => {
 	return (
 		<Fragment>
-			suggestions.length === 0 ? undefined : (
-			<div className='suggestion-parent'>
-				<Typography variant='h4'>
-					{sectionName} ({suggestions.length})
-				</Typography>
-				<div className='suggestion-wrapper'>{suggestions}</div>
-			</div>
+			{suggestions.length === 0 ? undefined : (
+				<div className='suggestion-parent'>
+					<Typography variant='h4'>
+						{sectionName} ({suggestions.length})
+					</Typography>
+					<div className='suggestion-wrapper'>{suggestions}</div>
+				</div>
+			)}
 		</Fragment>
 	)
 }
