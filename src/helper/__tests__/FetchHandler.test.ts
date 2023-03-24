b8bb013cfba897e065f54d2265388652be168765
Javi <rtomyj@gmail.com>
2023-03-23 23:26:04 -0500

    Fixed tests and imporved coverage

--- src/helper/__tests__/FetchHandler.test.ts
+++ src/helper/__tests__/FetchHandler.test.ts
@@ -1,9 +1,10 @@
-import { AxiosError, AxiosResponse } from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 import AppRoutes from '../AppRoutes'
 import FetchHandler from '../FetchHandler'
 
 beforeEach(() => {
-	global.window = Object.create(window)
+	// below line is needed in certain node versions...
+	// global.window = Object.create(window)
 	Object.defineProperty(window, 'location', {
 		value: {
 			href: '',
@@ -35,6 +36,12 @@ test('verify user is redirected to 408 page on Request Aborted Error', () => {
 	expect(location.href).toBe(AppRoutes.RequestTimeout)
 })
 
+test('handle request cancelled', () => {
+	const err = new axios.CanceledError('aborted')
+
+	FetchHandler.handleError(err)
+})
+
 test('verify user is redirected to 404-Server page on 404 error from API call', () => {
 	const err = new AxiosError()
 	err.response = { status: 404 } as AxiosResponse
@@ -49,4 +56,12 @@ test('verify user is redirected to 400 page on 400 error from API call', () => {
 	FetchHandler.handleError(err)
 
 	expect(location.href).toBe(AppRoutes.BadRequest)
+})
+
+test('verify user is redirected to GenericServerPage when server returns with non 400 or 404 error', () => {
+	const err = new AxiosError()
+	err.response = { status: 500 } as AxiosResponse
+	FetchHandler.handleError(err)
+
+	expect(location.href).toBe(AppRoutes.GenericServerError)
 })
