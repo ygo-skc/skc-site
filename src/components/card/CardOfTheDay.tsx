a25aef572dd489c4d3d42cbc050f06f9a2103f14
Javi <rtomyj@gmail.com>
2023-07-06 14:15:40 -0500

    cleanup - added error handler

--- src/components/card/CardOfTheDay.tsx
+++ src/components/card/CardOfTheDay.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from 'react'
 import FetchHandler from '../../helper/FetchHandler'
 import DownstreamServices from '../../helper/DownstreamServices'
-import { CardImageRounded, InlineDate, Section, YGOCardColorIndicator } from 'skc-rcl'
+import { CardImageRounded, GenericNonBreakingErr, InlineDate, Section, YGOCardColorIndicator } from 'skc-rcl'
 import { Skeleton, Typography } from '@mui/material'
 import '../../css/card/card-of-the-day.css'
 import { Dates } from '../../helper/Dates'
@@ -16,6 +16,7 @@ export default function CardOfTheDay() {
 	const [cardOfTheDay, setCardOfTheDay] = useState<SKCCard | undefined>(undefined)
 	const [date, setDate] = useState(new Date())
 	const [isLoading, setIsLoading] = useState(true)
+	const [hasError, setHasError] = useState(false)
 
 	useEffect(() => {
 		FetchHandler.handleFetch(
@@ -26,36 +27,41 @@ export default function CardOfTheDay() {
 				setIsLoading(false)
 			},
 			false
-		)?.catch((_err) => {})
+		)?.catch((_err) => {
+			setHasError(true)
+		})
 	}, [])
 
 	return (
 		<Section sectionName='Suggestions'>
-			<div onClick={() => window.location.assign(`/card/${cardOfTheDay?.cardID}`)} className='section-content card-of-the-day-parent'>
-				<Typography variant='h5'>Card of The Day</Typography>
-				<div className='card-of-the-day-wrapper'>
-					<div className='card-of-the-day-image'>
-						{!isLoading && <CardImageRounded cardImg={`https://images.thesupremekingscastle.com/cards/tn/${cardOfTheDay?.cardID}.jpg`} />}
-						{isLoading && <Skeleton className='rounded-skeleton' variant='circular' width='100%' height='100%' />}
-					</div>
-					<div className='card-of-the-day-data'>
-						{!isLoading && (
-							<Fragment>
-								<InlineDate month={Dates.getMonth(date)} day={+Dates.getDay(date)} year={+Dates.getYear(date)} />
-								<Typography variant='h5' className='card-of-the-day-text'>
-									{cardOfTheDay?.cardName}
-								</Typography>
-								<div className='card-of-the-day-wrapper'>
-									<YGOCardColorIndicator cardColor={cardOfTheDay?.cardColor} variant={'small'} />
-									<Typography variant='h6' className='card-of-the-day-text'>
-										{cardOfTheDay?.monsterType === undefined ? cardOfTheDay?.cardColor : cardOfTheDay.monsterType}
-									</Typography>
-								</div>
-							</Fragment>
-						)}
-						{isLoading && <Skeleton className='rounded-skeleton' variant='rectangular' width='100%' height='8rem' />}
-					</div>
-				</div>
+			<div onClick={() => window.location.assign(`/card/${cardOfTheDay?.cardID}`)} className={`section-content ${hasError ? '' : 'card-of-the-day-parent'}`} id='card-of-the-day'>
+				{(!hasError && (
+					<Fragment>
+						<Typography variant='h5'>Card of The Day</Typography>
+						<div className='card-of-the-day-wrapper'>
+							{(!isLoading && <CardImageRounded cardImg={`https://images.thesupremekingscastle.com/cards/tn/${cardOfTheDay?.cardID}.jpg`} />) || (
+								<Skeleton className='rounded-skeleton' variant='circular' />
+							)}
+
+							<div className='card-of-the-day-data'>
+								{(!isLoading && (
+									<Fragment>
+										<InlineDate month={Dates.getMonth(date)} day={+Dates.getDay(date)} year={+Dates.getYear(date)} />
+										<Typography variant='h5' className='card-of-the-day-text'>
+											{cardOfTheDay?.cardName}
+										</Typography>
+										<div className='card-of-the-day-type-wrapper'>
+											<YGOCardColorIndicator cardColor={cardOfTheDay?.cardColor} variant={'small'} />
+											<Typography variant='h6' className='card-of-the-day-text'>
+												{cardOfTheDay?.monsterType === undefined ? cardOfTheDay?.cardColor : cardOfTheDay.monsterType}
+											</Typography>
+										</div>
+									</Fragment>
+								)) || <Skeleton className='rounded-skeleton' variant='rectangular' width='100%' height='7rem' />}
+							</div>
+						</div>
+					</Fragment>
+				)) || <GenericNonBreakingErr errExplanation='Come back at a different time to see todays card of the day!' />}
 			</div>
 		</Section>
 	)
