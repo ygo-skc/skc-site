69e1e503c6c3dce2864526d9ff41539ee7822625
Javi <8519394+rtomyj@users.noreply.github.com>
2023-10-31 21:39:07 -0500

    Added sections where each section is a year

--- src/components/pages/ProductBrowse.tsx
+++ src/components/pages/ProductBrowse.tsx
@@ -14,48 +14,50 @@ import Grid2 from '@mui/material/Unstable_Grid2'
 const Breadcrumb = lazy(() => import('../header-footer/Breadcrumb'))
 
 const ProductBrowse: FunctionComponent = () => {
-	const [productJson, setProductJson] = useState<ProductInfo[]>([])
 	const [productGridItems, setProductGridItems] = useState<JSX.Element[]>([])
 	const [isDataLoaded, setIsDataLoaded] = useState(false)
 
 	useEffect(() => {
-		FetchHandler.handleFetch<ProductBrowseResults>(DownstreamServices.NAME_maps_ENDPOINT['productBrowse'], (json) => {
+		FetchHandler.handleFetch<ProductBrowseResults>(DownstreamServices.NAME_maps_ENDPOINT['productBrowse'], (json: ProductBrowseResults) => {
 			startTransition(() => {
-				setProductJson(json.products)
+				// setProductJson(json.products)
+				const x = json.products.reduce((map: Map<string, JSX.Element[]>, product: ProductInfo) => {
+					const productReleaseDate = Dates.fromYYYYMMDDToDate(product.productReleaseDate)
+
+					const yearStr = Dates.getYear(productReleaseDate)
+					map.set(yearStr, map.get(yearStr) ?? [])
+					map.get(yearStr)!.push(
+						<Grid2 key={product.productId} className='list-item-parent' onClick={() => window.location.assign(`/product/${product.productId}`)} xs={12} sm={6} md={4} lg={4} xl={3}>
+							<DateComponent month={Dates.getMonth(productReleaseDate)} day={+Dates.getDay(productReleaseDate)} year={+Dates.getYear(productReleaseDate)} variant='condensed' />
+							<div className='list-item-text'>
+								<Typography variant='body1'>
+									{product.productId} &#x25cf; {product.productTotal} Cards
+								</Typography>
+								<Typography variant='subtitle1'>{product.productName}</Typography>
+								<div>
+									<Chip className='dark-chip-condensed' key={product.productType} label={product.productType} />
+									<Chip className='dark-chip-condensed' key={product.productSubType} label={product.productSubType} />
+								</div>
+							</div>
+						</Grid2>
+					)
+					return map
+				}, new Map<string, JSX.Element[]>())
+
+				const y = Array.from(x.keys()).map((year: string) => {
+					return (
+						<div key={year} style={{ marginBottom: '2rem' }}>
+							<Typography variant='h2'>{year}</Typography>
+							<Grid2 container>{x.get(year)}</Grid2>
+						</div>
+					)
+				})
+
+				setProductGridItems(y)
+				setIsDataLoaded(true)
 			})
 		})
 	}, [])
-
-	useEffect(() => {
-		const x: JSX.Element[] = productJson.map((product: ProductInfo): JSX.Element => {
-			const productReleaseDate = Dates.fromYYYYMMDDToDate(product.productReleaseDate)
-
-			return (
-				<Grid2 key={product.productId} className='list-item-parent' onClick={() => window.location.assign(`/product/${product.productId}`)} xs={12} sm={6} md={4} lg={4} xl={3}>
-					{' '}
-					<DateComponent month={Dates.getMonth(productReleaseDate)} day={+Dates.getDay(productReleaseDate)} year={+Dates.getYear(productReleaseDate)} variant='condensed' />
-					<div className='list-item-text'>
-						<Typography variant='body1'>
-							{product.productId} &#x25cf; Total Cards - {product.productTotal}
-						</Typography>
-						<Typography variant='subtitle1'>{product.productName}</Typography>
-						<div>
-							<Typography variant='body1' className='rarities'>
-								Product Types
-							</Typography>
-							<Chip className='dark-chip-condensed' key={product.productType} label={product.productType} />
-							<Chip className='dark-chip-condensed' key={product.productSubType} label={product.productSubType} />
-						</div>
-					</div>
-				</Grid2>
-			)
-		})
-
-		startTransition(() => {
-			setProductGridItems(x)
-			setIsDataLoaded(true)
-		})
-	}, [productJson])
 
 	return (
 		<div className='generic-container'>
@@ -69,9 +71,8 @@ const ProductBrowse: FunctionComponent = () => {
 
 			<Section sectionHeaderBackground='product' sectionName='Products In Database'>
 				<div className='section-content'>
-					<Typography variant='h5'>Sorted By Release Date</Typography>
 					{!isDataLoaded && <Skeleton variant='rectangular' height='500px' width='100%' className='rounded-skeleton' />}
-					{isDataLoaded && <Grid2 container>{productGridItems}</Grid2>}
+					{isDataLoaded && productGridItems}
 				</div>
 			</Section>
 		</div>
