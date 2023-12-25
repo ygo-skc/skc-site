865a9f93f5fdc06a94f88f34e742fcbd39e1ed15
Javi <8519394+rtomyj@users.noreply.github.com>
2023-12-25 16:34:38 -0600

    Updating click event to anchor

--- src/components/product/ProductGridItem.tsx
+++ src/components/product/ProductGridItem.tsx
@@ -15,22 +15,24 @@ type ProductGridItemProps = {
 
 const ProductGridItem: FC<ProductGridItemProps> = ({ id, name, type, subType, totalItems, releaseDate }) => {
 	return (
-		<Grid2 key={id} className='list-item-parent' onClick={() => window.location.assign(`/product/${id}`)} xs={12} sm={6} md={4} lg={4} xl={3}>
-			<div className='product-img'>
-				<ProductImage variant='sm' productID={id} loading='lazy' />
-			</div>
-
-			<div className='list-item-text'>
-				<InlineDate month={Dates.getMonth(releaseDate)} day={+Dates.getDay(releaseDate)} year={+Dates.getYear(releaseDate)} />
-				<Typography variant='body1'>
-					{id} &#x25cf; {totalItems} Cards
-				</Typography>
-				<Typography variant='h5'>{name}</Typography>
-				<div>
-					<Chip className='dark-chip-condensed' key={type} label={type} />
-					<Chip className='dark-chip-condensed' key={subType} label={subType} />
+		<Grid2 key={id} xs={12} sm={6} md={4} lg={4} xl={3}>
+			<a href={`/product/${id}`} className='list-item-parent aggregate-anchor'>
+				<div className='product-img'>
+					<ProductImage variant='sm' productID={id} loading='lazy' />
 				</div>
-			</div>
+
+				<div className='list-item-text'>
+					<InlineDate month={Dates.getMonth(releaseDate)} day={+Dates.getDay(releaseDate)} year={+Dates.getYear(releaseDate)} />
+					<Typography variant='body1'>
+						{id} &#x25cf; {totalItems} Cards
+					</Typography>
+					<Typography variant='h5'>{name}</Typography>
+					<div>
+						<Chip className='dark-chip-condensed' key={type} label={type} />
+						<Chip className='dark-chip-condensed' key={subType} label={subType} />
+					</div>
+				</div>
+			</a>
 		</Grid2>
 	)
 }
