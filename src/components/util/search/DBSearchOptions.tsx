b6b6d3b31af1ae576c4884a1aa56ec132d1a3580
Javi <8519394+rtomyj@users.noreply.github.com>
2024-02-22 00:07:44 -0600

    Updating search term hi light logic

--- src/components/util/search/DBSearchOptions.tsx
+++ src/components/util/search/DBSearchOptions.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Typography } from '@mui/material'
-import { FC, memo, useCallback, useEffect, useState } from 'react'
+import React, { FC, Fragment, memo, useCallback, useEffect, useState } from 'react'
 
 type DBSearchOptionsProps = {
 	props: React.HTMLAttributes<HTMLLIElement>
@@ -11,17 +11,33 @@ type DBSearchOptionsProps = {
 
 const DBSearchOptions: FC<DBSearchOptionsProps> = memo(
 	({ props, searchSubject, cardNameOption, cardIdOption, monsterTypeOption }) => {
-		const UPPERCASE_CARD_NAME = cardNameOption.toUpperCase()
-		const UPPERCASE_SEARCH_TERM = searchSubject.toUpperCase()
-
-		const INDEX_OF_SEARCH_TERM = UPPERCASE_CARD_NAME.indexOf(UPPERCASE_SEARCH_TERM)
-		const LENGTH_OF_SEARCH_TERM = UPPERCASE_SEARCH_TERM.length
-
-		const [imgUrl, setImgUrl] = useState('')
+		const [imgUrl, setImgUrl] = useState(`https://images.thesupremekingscastle.com/cards/tn/${cardIdOption}.jpg`)
+		const [searchSuggestionText, setSearchSuggestionText] = useState<JSX.Element[]>([])
 
 		useEffect(() => {
-			setImgUrl(`https://images.thesupremekingscastle.com/cards/tn/${cardIdOption}.jpg`)
-		}, [cardIdOption])
+			const UPPERCASE_SEARCH_TERMS = searchSubject.toUpperCase().trim().split(' ')
+
+			setSearchSuggestionText(
+				cardNameOption.split(' ').map((token, ind) => {
+					const UPPERCASE_TOKEN = token.toUpperCase()
+					const MATCHED_TERM = UPPERCASE_SEARCH_TERMS.find((searchTerm) => UPPERCASE_TOKEN.includes(searchTerm))
+
+					if (MATCHED_TERM == undefined) {
+						return <span key={token}>{ind > 0 ? ` ${token}` : token}</span>
+					}
+
+					const INDEX_OF_SEARCH_TERM = UPPERCASE_TOKEN.indexOf(MATCHED_TERM)
+					const LENGTH_OF_SEARCH_TERM = MATCHED_TERM.length
+					return (
+						<Fragment key={token}>
+							<span>{ind > 0 ? ` ${token.slice(0, INDEX_OF_SEARCH_TERM)}` : token.slice(0, INDEX_OF_SEARCH_TERM)}</span>
+							<strong className='search-suggestion-substring-match'>{token.slice(INDEX_OF_SEARCH_TERM, INDEX_OF_SEARCH_TERM + LENGTH_OF_SEARCH_TERM)}</strong>
+							<span>{token.slice(INDEX_OF_SEARCH_TERM + LENGTH_OF_SEARCH_TERM)}</span>
+						</Fragment>
+					)
+				})
+			)
+		}, [cardNameOption, searchSubject])
 
 		const onAvatarImgLoadErrorCB = useCallback(() => {
 			setImgUrl('https://images.thesupremekingscastle.com/cards/tn/default-card-image.jpg')
@@ -29,12 +45,10 @@ const DBSearchOptions: FC<DBSearchOptionsProps> = memo(
 
 		return (
 			<li {...props} className='search-suggestions-parent'>
-				<Avatar className='card-image-avatar' alt={`${cardNameOption}-Avatar`} src={imgUrl} imgProps={{ onError: onAvatarImgLoadErrorCB }} />
+				<Avatar className='card-image-avatar' alt={`${cardNameOption}-Avatar`} src={imgUrl} slotProps={{ img: { onError: onAvatarImgLoadErrorCB } }} />
 				<div className='search-suggestions-info-parent'>
 					<Typography className='search-suggestion-text' variant='subtitle1'>
-						{cardNameOption.slice(0, INDEX_OF_SEARCH_TERM)}
-						<strong className='search-suggestion-substring-match'>{cardNameOption.slice(INDEX_OF_SEARCH_TERM, INDEX_OF_SEARCH_TERM + LENGTH_OF_SEARCH_TERM)}</strong>
-						{cardNameOption.slice(INDEX_OF_SEARCH_TERM + LENGTH_OF_SEARCH_TERM)}
+						{searchSuggestionText}
 					</Typography>
 					<Typography variant='body1' className='search-suggestion-text search-suggestion-subheader'>
 						{monsterTypeOption}
