8ef960b0110e70807060f18940416568a1e7d4b4
Javi <8519394+rtomyj@users.noreply.github.com>
2024-03-27 20:58:56 -0500

    Updated config

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,18 @@ fi
 
 SERVER=$1
 USER="ec2-user"
-DIR_ON_SERVER="skc-site"
+DIR_ON_SERVER="spa/skc-site"
 
 echo "Using server $SERVER and directory $DIR_ON_SERVER to sync prod API"
 
 echo "Uploading API files"
-rsync -avzh --delete --progress --delete -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yaml nginx.conf build "${USER}@${SERVER}:${DIR_ON_SERVER}/"
+rsync --rsync-path="mkdir -p ${DIR_ON_SERVER} && rsync" -avzh --delete --progress --delete -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yaml nginx.conf build "${USER}@${SERVER}:${DIR_ON_SERVER}/"
 
 echo -e "\n\nRestaging API"
 ssh -i ~/.ssh/skc-server.pem "${USER}@${SERVER}" << EOF
-	cd skc-site
+	cd "$DIR_ON_SERVER"
 	docker-compose kill
 	docker-compose rm -f
 	docker-compose pull
 	docker-compose up -d
 EOF
-
